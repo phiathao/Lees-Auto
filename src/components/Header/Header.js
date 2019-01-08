932cba577d9d65a60eeb22ce374858ecaf8d308e
phiathao <phiat11@gmail.com>
2019-01-08 11:37:21 -0600

    nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,34 +4,36 @@ import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Header.css';
 
-const Nav = (props) => (
-  <div className="header">
-    <Link to="/home">
-      <h2 className="nav-title">Lee's Auto Shop &amp; Repair</h2>
-    </Link>
-    <div className="nav-right">
-      <Link className="nav-link" to="/home">
-        {/* Show this link if they are logged in or not,
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <Link to="/home" className="header-title">
+          <h2>Lee's Auto Shop &amp; Repair</h2>
+        </Link>
+        <Link className="nav-link" to="/home">
+          {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
-      </Link>
-      {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
-        <>
-          <Link className="nav-link" to="/info">
-            Info Page
+          {this.props.user.id ? 'Home' : 'Login / Register'}
+        </Link>
+        {/* Show the link to the info page and the logout button if the user is logged in */}
+        {this.props.user.id && (
+          <>
+            <Link className="nav-link" to="/info">
+              Info Page
           </Link>
-          <LogOutButton className="nav-link"/>
-        </>
-      )}
-      {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
-        About
+            <LogOutButton className="nav-link" />
+          </>
+        )}
+        {/* Always show this link since the about page is not protected */}
+        <Link className="nav-link" to="/about">
+          About
       </Link>
-    </div>
-  </div>
-);
+      </div>
+    )
+  }
+}
 
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
@@ -42,4 +44,4 @@ const mapStateToProps = state => ({
   user: state.user,
 });
 
-export default connect(mapStateToProps)(Nav);
+export default connect(mapStateToProps)(Header);
