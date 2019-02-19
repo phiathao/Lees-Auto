c9765be475f2bc807b30e531a9805783b5657a68
phiathao <phiat11@gmail.com>
2019-02-18 19:15:54 -0600

    for deploy

---
+++ build/precache-manifest.10eddef75e6c9cfa8f5bb6ef8d45e214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa1a8af8c39a0568cd9c",
+    "url": "/static/css/main.8d2eaf0c.chunk.css"
+  },
+  {
+    "revision": "aa1a8af8c39a0568cd9c",
+    "url": "/static/js/main.aa1a8af8.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fea7c0ab5b6718fd7216",
+    "url": "/static/js/2.fea7c0ab.chunk.js"
+  },
+  {
+    "revision": "0566631011bc85aea20ac56a73f18ec7",
+    "url": "/index.html"
+  }
+];
